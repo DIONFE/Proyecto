e4ef403b8201545e2f7e074694a620bab5ff63c6
ElPajuelas <danielcanchon6@gmail.com>
2024-11-22 10:44:28 -0500

    1-Falta una seccion en la tabla de poblacion mas   vulnerable ejemplo:Asma poblacion 45 años en el orinoco 2. Falta una seccion acerca de la prevencion de la enfermedad

--- Mrlolazo546Place´s.py
+++ Mrlolazo546Place´s.py
@@ -1,4 +1,3 @@
-#hola cómo estás
 from tabulate import tabulate
 def autores():
     print(f"Codigo creado por:\nDaniel Canchon \nLuis Neira\nKevin Herrera\nDilan Hernández")
@@ -9,8 +8,8 @@ print(
 |   predicción de enfermedades   |
 |       del grupo número 5!      |
 |    Aquí consultarás ciertas    |
-|    enfermedades de interés     |
-|    A continuación, revisa:     |
+|          enfermedades          |
+|        A continuación:         |
 +================================+
 """)
 
@@ -38,69 +37,69 @@ enfermedades = {
     "enfermedad pulmonar": {
         "nombre": "Enfermedad Pulmonar Obstructiva Crónica (EPOC)",
         "causa": "Principalmente causada por el tabaquismo, también exposición a contaminantes, polvo y productos químicos",
-        "síntomas": "Tos crónica, producción de esputo, dificultad para respirar, sibilancias, fatiga",
-        "diagnóstico": "Pruebas de función pulmonar (espirometría), radiografías de tórax, análisis de gases en sangre arterial",
-        "tratamiento": "Dejar de fumar, broncodilatadores, corticosteroides inhalados, terapia de oxígeno, rehabilitación pulmonar",
+        "síntomas": "1.Tos crónica\n2.producción de esputo\n3.dificultad para respirar\n4.sibilancias\n5.fatiga",
+        "diagnóstico": "1.Pruebas de función pulmonar (espirometría)\n2.radiografías de tórax\n3.análisis de gases en sangre arterial",
+        "tratamiento": "1.Dejar de fumar\n2.broncodilatadores\n3.corticosteroides inhalados\n4.terapia de oxígeno\n5.rehabilitación pulmonar",
         "material de apoyo": "https://www.who.int/news-room/fact-sheets/detail/chronic-obstructive-pulmonary-disease-(copd)"
 
     },
      "asma": {
         "nombre": "Asma",
         "causa": "Inflamación y estrechamiento de las vías respiratorias, desencadenada por factores como alérgenos, infecciones respiratorias, ejercicio o estrés",
-        "síntomas": "Dificultad para respirar, sibilancias, opresión en el pecho, tos, especialmente por la noche o temprano en la mañana",
-        "diagnóstico": "Pruebas de función pulmonar (espirometría), pruebas de alergia, medición del óxido nítrico exhalado",
-        "tratamiento": "Inhaladores (broncodilatadores y corticosteroides), medicamentos de control a largo plazo, evitar desencadenantes, planes de acción para el asma",
+        "síntomas": "1.Dificultad para respirar\n2.sibilancias\n3.opresión en el pecho\n4.tos, especialmente por la noche o temprano en la mañana",
+        "diagnóstico": "1.Pruebas de función pulmonar (espirometría)\n2.pruebas de alergia\n3.medición del óxido nítrico exhalado",
+        "tratamiento": "1.Inhaladores (broncodilatadores y corticosteroides),\n2.medicamentos de control a largo plazo\n3.evitar desencadenantes\n4.planes de acción para el asma",
         "material de apoyo": "https://www.who.int/news-room/fact-sheets/detail/asthma"
 
     },
      "hepatitis": {
         "nombre": "Hepatitis",
         "causa": "Infección viral (hepatitis A, B, C, D, E), consumo excesivo de alcohol, toxinas, enfermedades autoinmunes",
-        "síntomas": "Fatiga, náuseas, vómitos, dolor abdominal, ictericia (piel y ojos amarillos), orina oscura",
-        "diagnóstico": "Pruebas de sangre para detectar la presencia del virus, ecografía del hígado, biopsia hepática",
-        "tratamiento": "Antivirales, interferón, cambios en el estilo de vida, evitar el consumo de alcohol, en casos graves, trasplante de hígado",
+        "síntomas": "1.Fatiga\n2.náuseas\n3.vómitos\n4.dolor abdominal\n5.ictericia (piel y ojos amarillos)\n6.orina oscura",
+        "diagnóstico": "1.Pruebas de sangre para detectar la presencia del virus\n2.ecografía del hígado\n3.biopsia hepática",
+        "tratamiento": "1.Antivirales\n2.interferón\n3.cambios en el estilo de vida\n4.evitar el consumo de alcohol\n5.en casos graves\n6.trasplante de hígado",
         "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/hepatitis"
 
     },
      "obesidad": {
         "nombre": "Obesidad",
         "causa": "Desequilibrio entre la ingesta calórica y el gasto energético, factores genéticos, estilo de vida sedentario, mala alimentación",
-        "síntomas": "Exceso de grasa corporal, dificultad para realizar actividades físicas, problemas respiratorios, fatiga",
-        "diagnóstico": "Índice de masa corporal (IMC), medición del perímetro abdominal, análisis de composición corporal",
-        "tratamiento": "Dieta equilibrada, ejercicio regular, cambios en el estilo de vida, en algunos casos, medicamentos o cirugía bariátrica",
+        "síntomas": "1.Exceso de grasa corporal\n2.dificultad para realizar actividades físicas\n3.problemas respiratorios\n4.fatiga",
+        "diagnóstico": "1.Índice de masa corporal (IMC)\n2.medición del perímetro abdominal\n3.análisis de composición corporal",
+        "tratamiento": "1.Dieta equilibrada\n2.ejercicio regular\n3.cambios en el estilo de vida\n4.en algunos casos, medicamentos o cirugía bariátrica",
         "material de apoyo": "https://www.who.int/topics/obesity/es/"
 
     },
      "dengue": {
       "nombre": "Dengue",
         "causa": "Virus del dengue, transmitido por mosquitos infectados (Aedes aegypti)",
-        "síntomas": "Fiebre alta, dolor de cabeza, dolor detrás de los ojos, dolor muscular y articular, náuseas, vómitos, erupciones cutáneas",
-        "diagnóstico": "Pruebas de sangre para detectar la presencia del virus, frotis de sangre, pruebas rápidas de diagnóstico",
-        "tratamiento": "Medicamentos para aliviar el dolor (acetaminofén), hospitalización en casos graves, hidratación y reposo",
+        "síntomas": "1.Fiebre alta\n2.dolor de cabeza\n3.Dolor detrás de los ojos, dolor muscular y articular\n4.náuseas\n5.vómitos\n6.erupciones cutáneas",
+        "diagnóstico": "1.Pruebas de sangre para detectar la presencia del virus\n2.frotis de sangre\n3.pruebas rápidas de diagnóstico",
+        "tratamiento": "1.Medicamentos para aliviar el dolor (acetaminofén)\n2.hospitalización en casos graves\n3.hidratación y reposo",
         "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/dengue-and-severe-dengue"
     },
      "anemia": {
         "nombre": "Anemia",
         "causa": "Deficiencia de hierro, deficiencia de vitamina B12, pérdida de sangre, enfermedades crónicas",
-        "síntomas": "Fatiga, debilidad, piel pálida, dificultad para respirar, mareos, dolores de cabeza",
-        "diagnóstico": "Análisis de sangre (hemograma completo), niveles de hierro, ferritina y vitamina B12",
-        "tratamiento": "Suplementos de hierro, vitamina B12, cambio en la dieta, tratamiento de la causa subyacente",
+        "síntomas": "1.Fatiga\n2.debilidad\n3.piel pálida\n4.dificultad para respirar\n5.mareos\n6.dolores de cabeza",
+        "diagnóstico": "1.Análisis de sangre (hemograma completo)\n2.niveles de hierro ferritina y vitamina B12",
+        "tratamiento": "1.Suplementos de hierro\n2.vitamina B12\n3.cambio en la dieta\n4.tratamiento de la causa subyacente",
         "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/anaemia"
 
     }, "malaria": {
         "nombre": "Malaria",
         "causa": "Plasmodium (parásitos), transmitidos por mosquitos Anopheles infectados",
-        "síntomas": "Fiebre, escalofríos, sudoración, dolor de cabeza, náuseas, vómitos, diarrea, anemia",
-        "diagnóstico": "Pruebas de sangre para detectar la presencia de parásitos, frotis de sangre, pruebas rápidas de diagnóstico",
-        "tratamiento": "Medicamentos antimaláricos como la cloroquina, artemisinina, seguimiento médico para complicaciones",
+        "síntomas": "1.Fiebre\n2.escalofríos\n3.sudoración\n4.dolor de cabeza\n5.náuseas\n6.vómitos\n7.diarrea\n8.anemia",
+        "diagnóstico": "1.Pruebas de sangre para detectar la presencia de parásitos\n2.frotis de sangre\n3.pruebas rápidas de diagnóstico",
+        "tratamiento": "1.Medicamentos antimaláricos como la cloroquina\n2.artemisinina\n3.seguimiento médico para complicaciones",
         "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/malaria"
 
     }, "sida": {
         "nombre": "VIH/SIDA",
         "causa": "Virus de la inmunodeficiencia humana (VIH)",
-        "síntomas": "Fiebre, fatiga, inflamación de los ganglios linfáticos, pérdida de peso, infecciones oportunistas",
-        "diagnóstico": "Pruebas de detección de anticuerpos del VIH, pruebas de carga viral, pruebas de CD4",
-        "tratamiento": "Terapia antirretroviral (TAR), medicamentos para infecciones oportunistas, seguimiento médico regular",
+        "síntomas": "1.Fiebre\n2.fatiga\n3.inflamación de los ganglios linfáticos\n4.pérdida de peso\n5.infecciones oportunistas",
+        "diagnóstico": "1.Pruebas de detección de anticuerpos del VIH\n2.pruebas de carga viral\n3.pruebas de CD4",
+        "tratamiento": "1.Terapia antirretroviral (TAR\n2.medicamentos para infecciones oportunistas\n3.seguimiento médico regular",
         "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/hiv-aids"
 
     }
