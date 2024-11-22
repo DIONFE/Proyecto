22134ed52b43262127115bf86c27259e38ee2f23
elbestopocket <alejoneira29xdd@gmail.com>
2024-11-21 23:22:20 -0500

    Cambios código de Daniel

--- Mrlolazo546Place´s.py
+++ Mrlolazo546Place´s.py
@@ -1,5 +1,7 @@
 #hola cómo estás
 from tabulate import tabulate
+def autores():
+    print(f"Codigo creado por:\nDaniel Canchon \nLuis Neira\nKevin Herrera\nDilan Hernández")
 print(
 """
 +================================+
@@ -8,7 +10,7 @@ print(
 |       del grupo número 5!      |
 |    Aquí consultarás ciertas    |
 |    enfermedades de interés     |
-|    A continuación, ingresa:    |
+|    A continuación, revisa:     |
 +================================+
 """)
 
@@ -16,30 +18,30 @@ enfermedades = {
     "diabetes": {
         "nombre": "Diabetes",
         "causa": "Insuficiencia de insulina o resistencia a la insulina en el cuerpo",
-        "síntomas": "Sed excesiva, micción frecuente, hambre extrema, pérdida de peso inexplicada, fatiga",
-        "diagnóstico": "Pruebas de glucosa en sangre, prueba de A1C, pruebas de tolerancia a la glucosa",
-        "tratamiento": "Insulina, medicamentos orales, dieta saludable, ejercicio regular, monitoreo constante de los niveles de glucosa",
-        "link": "https://www.who.int/news-room/fact-sheets/detail/diabetes"
+        "síntomas": "1. Sed excesiva \n2. Micción frecuente \n3. Hambre extrema \n4. Pérdida de peso inexplicable, \n5. Fatiga",
+        "diagnóstico": "1. Pruebas de glucosa en sangre \n2. Prueba de A1C \n3. Pruebas de tolerancia a la glucosa",
+        "tratamiento": "1. Insulina \n2. Medicamentos orales \n3. Dieta saludable \n4. Ejercicio regular \n5. Monitoreo constante de los niveles de glucosa",
+        "material de apoyo:": "https://www.who.int/news-room/fact-sheets/detail/diabetes"
 
         
         },
     "hipertension": {
         "nombre": "Hipertensión",
-        "causa": "Genética, dieta alta en sodio, obesidad, sedentarismo, consumo excesivo de alcohol, estrés",
-        "síntomas": "Generalmente asintomática en etapas tempranas, dolor de cabeza, dificultad para respirar, mareos, visión borrosa, palpitaciones",
-        "diagnóstico": "Medición de la presión arterial en múltiples ocasiones, monitoreo de la presión arterial en casa, pruebas de laboratorio y ecocardiogramas para detectar daño en órganos",
-        "tratamiento": "Modificaciones en el estilo de vida (dieta baja en sodio, ejercicio regular), medicamentos antihipertensivos, manejo del estrés, reducción del consumo de alcohol y tabaco",
-        "link": "https://www.who.int/news-room/fact-sheets/detail/hypertension"
+        "causa": "1. Genética \n2. Dieta alta en sodio \n3. Sobrepeso \n4. Sedentarismo \n5. Consumo excesivo de alcohol \n6. Estrés",
+        "síntomas": "1. Generalmente asintomática en etapas tempranas \n2. Dolor de cabeza \n3. Dificultad para respirar \n4. Mareos, \n5. Visión borrosa \n6. Palpitaciones",
+        "diagnóstico": "1. Medición de la presión arterial en múltiples ocasiones \n2. Monitoreo de la presión arterial en casa \n3. Pruebas de laboratorio y electrocardiogramas para detectar daño en órganos",
+        "tratamiento": "1. Modificaciones en el estilo de vida (dieta baja en sodio, ejercicio regular, etc.) \n2. Medicamentos antihipertensivos \n3. Manejo del estrés \n4. Reducción del consumo de alcohol y tabaco",
+        "material de apoyo": "https://www.who.int/news-room/fact-sheets/detail/hypertension"
 
 
     },
-    "enfermedad pulmonal": {
+    "enfermedad pulmonar": {
         "nombre": "Enfermedad Pulmonar Obstructiva Crónica (EPOC)",
         "causa": "Principalmente causada por el tabaquismo, también exposición a contaminantes, polvo y productos químicos",
         "síntomas": "Tos crónica, producción de esputo, dificultad para respirar, sibilancias, fatiga",
         "diagnóstico": "Pruebas de función pulmonar (espirometría), radiografías de tórax, análisis de gases en sangre arterial",
         "tratamiento": "Dejar de fumar, broncodilatadores, corticosteroides inhalados, terapia de oxígeno, rehabilitación pulmonar",
-        "link": "https://www.who.int/news-room/fact-sheets/detail/chronic-obstructive-pulmonary-disease-(copd)"
+        "material de apoyo": "https://www.who.int/news-room/fact-sheets/detail/chronic-obstructive-pulmonary-disease-(copd)"
 
     },
      "asma": {
@@ -48,7 +50,7 @@ enfermedades = {
         "síntomas": "Dificultad para respirar, sibilancias, opresión en el pecho, tos, especialmente por la noche o temprano en la mañana",
         "diagnóstico": "Pruebas de función pulmonar (espirometría), pruebas de alergia, medición del óxido nítrico exhalado",
         "tratamiento": "Inhaladores (broncodilatadores y corticosteroides), medicamentos de control a largo plazo, evitar desencadenantes, planes de acción para el asma",
-        "link": "https://www.who.int/news-room/fact-sheets/detail/asthma"
+        "material de apoyo": "https://www.who.int/news-room/fact-sheets/detail/asthma"
 
     },
      "hepatitis": {
@@ -57,7 +59,7 @@ enfermedades = {
         "síntomas": "Fatiga, náuseas, vómitos, dolor abdominal, ictericia (piel y ojos amarillos), orina oscura",
         "diagnóstico": "Pruebas de sangre para detectar la presencia del virus, ecografía del hígado, biopsia hepática",
         "tratamiento": "Antivirales, interferón, cambios en el estilo de vida, evitar el consumo de alcohol, en casos graves, trasplante de hígado",
-        "link": "https://www.who.int/es/news-room/fact-sheets/detail/hepatitis"
+        "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/hepatitis"
 
     },
      "obesidad": {
@@ -66,7 +68,7 @@ enfermedades = {
         "síntomas": "Exceso de grasa corporal, dificultad para realizar actividades físicas, problemas respiratorios, fatiga",
         "diagnóstico": "Índice de masa corporal (IMC), medición del perímetro abdominal, análisis de composición corporal",
         "tratamiento": "Dieta equilibrada, ejercicio regular, cambios en el estilo de vida, en algunos casos, medicamentos o cirugía bariátrica",
-        "link": "https://www.who.int/topics/obesity/es/"
+        "material de apoyo": "https://www.who.int/topics/obesity/es/"
 
     },
      "dengue": {
@@ -75,23 +77,23 @@ enfermedades = {
         "síntomas": "Fiebre alta, dolor de cabeza, dolor detrás de los ojos, dolor muscular y articular, náuseas, vómitos, erupciones cutáneas",
         "diagnóstico": "Pruebas de sangre para detectar la presencia del virus, frotis de sangre, pruebas rápidas de diagnóstico",
         "tratamiento": "Medicamentos para aliviar el dolor (acetaminofén), hospitalización en casos graves, hidratación y reposo",
-        "link": "https://www.who.int/es/news-room/fact-sheets/detail/dengue-and-severe-dengue"
+        "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/dengue-and-severe-dengue"
     },
-     "Anemia": {
+     "anemia": {
         "nombre": "Anemia",
         "causa": "Deficiencia de hierro, deficiencia de vitamina B12, pérdida de sangre, enfermedades crónicas",
         "síntomas": "Fatiga, debilidad, piel pálida, dificultad para respirar, mareos, dolores de cabeza",
         "diagnóstico": "Análisis de sangre (hemograma completo), niveles de hierro, ferritina y vitamina B12",
         "tratamiento": "Suplementos de hierro, vitamina B12, cambio en la dieta, tratamiento de la causa subyacente",
-        "link": "https://www.who.int/es/news-room/fact-sheets/detail/anaemia"
+        "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/anaemia"
 
-    }, "Malaria": {
+    }, "malaria": {
         "nombre": "Malaria",
         "causa": "Plasmodium (parásitos), transmitidos por mosquitos Anopheles infectados",
         "síntomas": "Fiebre, escalofríos, sudoración, dolor de cabeza, náuseas, vómitos, diarrea, anemia",
         "diagnóstico": "Pruebas de sangre para detectar la presencia de parásitos, frotis de sangre, pruebas rápidas de diagnóstico",
         "tratamiento": "Medicamentos antimaláricos como la cloroquina, artemisinina, seguimiento médico para complicaciones",
-        "link": "https://www.who.int/es/news-room/fact-sheets/detail/malaria"
+        "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/malaria"
 
     }, "sida": {
         "nombre": "VIH/SIDA",
@@ -99,7 +101,7 @@ enfermedades = {
         "síntomas": "Fiebre, fatiga, inflamación de los ganglios linfáticos, pérdida de peso, infecciones oportunistas",
         "diagnóstico": "Pruebas de detección de anticuerpos del VIH, pruebas de carga viral, pruebas de CD4",
         "tratamiento": "Terapia antirretroviral (TAR), medicamentos para infecciones oportunistas, seguimiento médico regular",
-        "link": "https://www.who.int/es/news-room/fact-sheets/detail/hiv-aids"
+        "material de apoyo": "https://www.who.int/es/news-room/fact-sheets/detail/hiv-aids"
 
     }
 }
@@ -111,21 +113,23 @@ def mostrar_informacion(enfermedad):
         data = [[key, value] for key, value in enfermedades[enfermedad].items()]
         print(tabulate(data, headers=["Campo", "Descripción"], tablefmt="grid"))
     else:
-        print("Enfermedad no encontrada. Por favor, elige entre: diabetes, hipertensión ,enfermedad pulmonar , asma ,hepatitis, obesidad, dengue, anemia, malaria ,sida")
+        print("Enfermedad no encontrada. Por favor, elige entre: Diabetes, hipertensión, enfermedad pulmonar, asma, hepatitis, obesidad, dengue, anemia, malaria, sida \n")
 contador=0
 while contador != 1:
-   tipo_de_enfermedad = int(input("Elige el tipo de enfermedad:\n1.Enfermedades    2.Enfermedades trasmisibles "))
+   tipo_de_enfermedad = int(input("Elige el tipo de enfermedad:\n1. Enfermedades metabólicas crónicas  |  2. Enfermedades trasmisibles \n "))
 
    if tipo_de_enfermedad == 1:
-      enfermedad_elegida = input("Digita la enfermedad la cual desea acceder al repositorio: diabetes, hipertension, asma, obesidad, anemia ").lower()
+      enfermedad_elegida = input("Digita la enfermedad con la cual desea acceder al repositorio: Diabetes, hipertensión, asma, obesidad, anemia \n").lower()
       mostrar_informacion(enfermedad_elegida)
-      contador = int(input("Si desea terminar el programa digite 1: "))
+      contador = int(input("Si desea terminar el programa digite 1, de otra manera digite cualquier otro número: "))
    elif tipo_de_enfermedad == 2:
-      enfermedad_elegida = input("Digita la enfermedad la cual desea acceder al repositorio: enfermedad pulmonar, hepariris, dengue, malaria, sida").lower()
+      enfermedad_elegida = input("Digite la enfermedad con la cual desea acceder al repositorio: Enfermedad pulmonar, hepatitis, dengue, malaria, sida \n").lower()
       mostrar_informacion(enfermedad_elegida)
-      contador = int(input("Si desea terminar el programa digite 1: "))
+      contador = int(input("Si desea terminar el programa digite 1, de otra manera digite cualquier otro número: "))
    else:
-      print("Digito un dato erroneo")
-      contador = int(input("Si desea terminar el programa digite 1: "))
+      print("Ha digitado un dato erróneo")
+      contador = int(input("Si desea terminar el programa digite 1, de otra manera digite cualquier otro número: "))
+      autores()
 
-print("Descanse")
+print("¡Gracias por usar el programa!")
+autores()
